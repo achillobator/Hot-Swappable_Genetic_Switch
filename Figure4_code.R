3099b6b96a2ee71dee1c318f8634793c0445c798
achillobator <achillobator@users.noreply.github.com>
2024-09-30 15:12:46 -0700

    Add files via upload

--- Figure4_code.R
+++ Figure4_code.R
@@ -2,28 +2,29 @@ library(ggthemes)
 library(ggplot2)
 library("viridis") 
 
-dat <- read.csv("LRDI_quantification.csv")
-dat <- LRDI_quantification
+dat_LRD <- read.csv("Isoswotch_LRD.csv")
+dat_LRD <- subset(dat_LRD, comments !=  "contaminated")
+dat_LRD <- subset(dat_LRD, switch !=  "none")
+dat_LRD <- subset(dat_LRD, Lrpheno !=  "yes")
+dat_LRD <- subset(dat_LRD, PRL > 40)
+dat_LRD$switch_geno <- factor(dat_LRD$switch_geno, levels=c("Wt","Del3","Del5","Del7"),ordered=TRUE)
 
 #This piece takes a data file and orders it based on the category after the money sign
 #dat$genotype <- factor(dat$genotype, levels=c("Col-0","5057","299","409","406","414","411","408","407","slr"),ordered=TRUE)
-A_colors <- c("#424242","#FF5733", "#FF5733","#FF9900", "#FF9900", "#fa9fb5", "#c51b8a","#0099FF","#0033CC","#a1d99b","#31a354","#bdbdbd","#636363")
+#A_colors <- c("#424242","#FF5733", "#FF5733","#FF9900", "#FF9900", "#fa9fb5", "#c51b8a","#0099FF","#0033CC","#a1d99b","#31a354","#bdbdbd","#636363")
 ##########
+A_colors <- c("#000000","#FFCC00", "#FF9900","#F71735","#bdbdbd","#636363", "#0099FF", "#c51b8a","#0099FF","#0033CC","#a1d99b","#31a354","#bdbdbd","#636363")
+
 #This plots all the data initially to ensure the data has been loaded in
-plot1<- ggplot(dat, x=Parent) + 
-  geom_boxplot(aes(x=Parent,y=as.numeric(LRILRDens)), outlier.size = 0, alpha = 0.5) 
-  
+plot1<- ggplot(dat_LRD, x=switch_geno) + 
+  geom_boxplot(aes(x=switch_geno,y=LRD), outlier.size = 0, alpha = 0.5) 
 plot1
 
-is.factor(dat$LRILRDens)
-is.factor(dat$Parent)
-
-
-plot1<- ggplot(dat_B, aes(x=Parent, y = as.numeric(LRILRDens), color=Parent)) + 
+plot1<- ggplot(dat_LRD, aes(x=switch_geno, y = as.numeric(LRD), color=switch_geno)) + 
   geom_boxplot(lwd=1, outlier.size = 0, alpha = 0.5) +
   geom_jitter(size =1, width = 0.1, alpha = 0.5) +
   #coord_cartesian(ylim = c(0.4, 1.25)) +
-  ylab('LRID') +
+  ylab('LRD') +
   theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), strip.background  = element_blank()) +
   theme_classic(base_size = 12)+
   scale_colour_manual(values = A_colors)+
@@ -32,26 +33,61 @@ plot1<- ggplot(dat_B, aes(x=Parent, y = as.numeric(LRILRDens), color=Parent)) +
   #facet_wrap(~Day, nrow=1) 
 plot1
 
-
-dat_B <- subset(dat, switch !=  "not-specific")
-dat_B <- subset(dat, PRL > 30)
-dat_B <- na.omit(dat_B)
-
-
-plot2<- ggplot(dat_B, aes(as.factor(Parent), y = PRL, color=Parent)) +
-  geom_boxplot(outlier.size = 0.1) + 
-  geom_jitter(size =1, width = 0.1) +
-  coord_cartesian(ylim = c(0, 125)) +
-  ylab('Root Length') +
+plot1_pheno<- ggplot(dat_LRD, aes(x= switch_geno, y = LRD)) + 
+  geom_boxplot(lwd=1, outlier.size = 0, alpha = 0.7, aes(color=switch_geno)) +
+  geom_jitter(size =2.5, width = 0.15, alpha = 0.8,aes(color=med21_genotype)) +
+  #coord_cartesian(ylim = c(0.2, .8)) +
+  ylab('LRD') +
   theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), strip.background  = element_blank()) +
   theme_classic(base_size = 10)+
   scale_colour_manual(values = A_colors)+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))+
   theme(legend.position="none")
+plot1_pheno
+plot1_pheno + facet_wrap(~med21_genotype)
 
-plot2
+####
+dat_LRL <- read.csv("Isoswotch_LRL.csv")
+#dat_LRL <- subset(dat_LRD, PRL > 40)
+dat_LRL$Parent <- factor(dat_LRL$Parent, levels=c("904","903","902","901"),ordered=TRUE)
 
-A_colors <- c("#bdbdbd","#636363","#000000","#F71735","#FFCC00", "#FF9900", "#fa9fb5", "#c51b8a","#0099FF","#0033CC","#a1d99b","#31a354","#bdbdbd","#636363")
+
+plot_LRL<- ggplot(dat_LRL, aes(x= Parent, y = LRL)) +
+  geom_violin(lwd=1, aes(color=Parent))+
+  #geom_boxplot(lwd=1, outlier.size = 0, alpha = 0.7, aes(color=Parent)) +
+  geom_jitter(size =1, width = 0.2, alpha = 0.5,aes(color=med21_genotype)) +
+  coord_cartesian(ylim = c(0, 40)) +
+  ylab('Lateral Root Length') +
+  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), strip.background  = element_blank()) +
+  theme_classic(base_size = 10)+
+  scale_colour_manual(values = A_colors)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(legend.position="none")
+plot_LRL
+plot_LRL+facet_wrap(~med21_genotype)
+
+plot_LRL2<- ggplot(dat_LRL, aes(x= med21_genotype, y = LRL)) +
+  geom_violin(lwd=1, aes(color=Parent))+
+  geom_boxplot(width=.1, lwd=1, outlier.size = 0, alpha = 0.7, aes(color=Parent)) +
+  #geom_jitter(size =1, width = 0.2, alpha = 0.5,aes(color=med21_genotype)) +
+  #coord_cartesian(ylim = c(0, 40)) +
+  ylab('Lateral Root Length') +
+  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), strip.background  = element_blank()) +
+  theme_classic(base_size = 10)+
+  scale_colour_manual(values = A_colors)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(legend.position="none")
+plot_LRL2
+plot_LRL2+facet_wrap(~Parent, nrow=1)
+
+####
+####old below
+
+
+
+
+
+
 
 dat_pheno <- read.csv("SPT6_R3_LRDdata_pooled_pheno.csv")
 plot1_pheno<- ggplot(dat_pheno, aes(x= genotype, y = LRD)) + 
@@ -141,33 +177,33 @@ plot3
 
 A_colors <- c("#424242","#424242","#424242","#424242","#FFCC00", "#FF9900", "#fa9fb5", "#c51b8a","#0099FF","#0033CC","#a1d99b","#31a354","#bdbdbd","#636363")
  ##########
-dat2 <- dat_pheno
+dat <- dat_LRD
 dat2 <- subset(dat2, Day == 14)
 dat_7 <- subset(dat2, comment !=  "fungus")
 write.csv(dat2, "dat2.csv")
 
 is.factor(LRDI_quantification$LRILRDens)
 is.factor(LRDI_quantification$Parent)
-dat <- LRDI_quantification_rep
+dat <- dat_LRD
 dat <- read.csv("LRDI_quantification_rep.csv")
 
 #Run ANNOVA
 attach(dat)
 data(dat)
 str(dat$dat)
-tapply(dat$Parent, dat$LRILRDens, dat$mean) 
-tapply(dat$Parent, dat$LRILRDens, dat$var)
-boxplot(LRILRDens ~ Parent, data=dat)
+tapply(dat$Parent, dat$LRD, dat$mean) 
+tapply(dat$Parent, dat$LRD, dat$var)
+boxplot(LRD ~ Parent, data=dat)
 
-lm.out = with(dat, lm(LRILRDens ~ Parent))
-aov.out = aov(LRILRDens ~ Parent, data=dat)
-oneway.test(LRILRDens ~ Parent, data=dat)
-is.factor(dat$LRILRDens)
+lm.out = with(dat, lm(LRD ~ Parent))
+aov.out = aov(LRD ~ Parent, data=dat)
+oneway.test(LRD ~ Parent, data=dat)
+is.factor(dat$LRD)
 is.factor(dat$Parent)
 dat2
 #View(sapply(dat2, class))
 dat$Parent <- factor(dat$Parent)
-dat$LRILRDens <- as.factor(dat$LRILRDens)
+dat$LRD <- as.factor(dat$LRD)
 
 aov.out
 summary(aov.out)
@@ -175,7 +211,7 @@ TukeyHSD(aov.out)
 
 summary.lm(aov.out)
 
-model=lm( LRILRDens ~ Parent, data=crop.data)
+model=lm( LRD ~ Parent, data=dat)
 ANOVA=aov(model)
 TUKEY <- TukeyHSD(x=ANOVA, 'crop.data$Parent', conf.level=0.95)
 plot(TUKEY , las=1 , col="brown")
@@ -199,23 +235,27 @@ library(tidyverse)
 library(broom)
 library(AICcmodavg)
 
-crop.data <- read.csv("LRDI_quantification_slim.csv", header = TRUE, colClasses = c("factor", "factor", "factor", "numeric", "numeric", "numeric"))
+crop.data <- read.csv("Isoswotch_LRD.csv", header = TRUE, colClasses = c("factor","numeric","numeric", "factor", "factor", "factor","numeric", "numeric", "numeric","numeric", "factor", "factor", "factor", "factor", "factor"))
+crop.data <- subset(crop.data, comments !=  "contaminated")
+crop.data <- subset(crop.data, switch !=  "none")
+crop.data <- subset(crop.data, Lrpheno !=  "yes")
+crop.data <- subset(crop.data, PRL > 40)
 summary(crop.data)
-one.way <- aov(LRILRDens ~ Parent, data = crop.data)
+one.way <- aov(LRD ~ Parent, data = crop.data)
 summary(one.way)
 tukey.one.way<-TukeyHSD(one.way)
 tukey.one.way
-tukey.plot.aov<-aov(LRILRDens ~ Parent, data=crop.data)
+tukey.plot.aov<-aov(LRD ~ Parent, data=crop.data)
 tukey.plot.test<-TukeyHSD(tukey.plot.aov)
 plot(tukey.plot.test, las = 1)
 mean.rld.data <- crop.data %>%
   group_by(Parent) %>%
   summarise(
-    LRD = mean(LRILRDens)
+    LRD = mean(LRD)
   )
-mean.rld.data$group <- c("a","b","b","b","b")
+mean.rld.data$group <- c("b","ab","b","a")
 mean.rld.data
-one.way.plot <- ggplot(crop.data, aes(x = Parent, y = LRILRDens)) +
+one.way.plot <- ggplot(crop.data, aes(x = Parent, y = LRD)) +
   geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))+
   stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
   stat_summary(fun.data = 'mean_se', geom = 'pointrange')+
@@ -236,7 +276,7 @@ mean.rld.data <- crop.data %>%
   summarise(
     PRL = mean(PRL)
   )
-mean.rld.data$group <- c("a","b","b","b","b")
+mean.rld.data$group <- c("a","a","a","a")
 mean.rld.data
 one.way.plot <- ggplot(crop.data, aes(x = Parent, y = PRL)) +
   geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))+
